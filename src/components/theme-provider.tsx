3fbe6d0bcc2570b173cf7e56bf8dc4ef6f6935cc
Adhvait Jadav <adhvait.jadav@gmail.com>
2025-11-20 00:28:56 -0800

    update

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -7,5 +7,14 @@ export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
